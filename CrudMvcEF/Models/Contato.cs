469311b14b6412b1b87b10a6cf7cbff4ac26c02a
Nailson Ferreira <nailson_ferreira@hotmail.com>
2019-03-17 00:32:42 -0300

    Models

---
+++ CrudMvcEF/Models/Contato.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudMvcEF.Models
+{
+    public class Contato
+    {
+        public int Id { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        
+        public Contato()
+        {
+
+        }
+
+        public Contato(int id, string telefone, string email)
+        {
+            Id = id;
+            Telefone = telefone;
+            Email = email;
+        }
+    }
+}
---
+++ CrudMvcEF/Models/Endereco.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudMvcEF.Models
+{
+    public class Endereco
+    {
+        public int Id { get; set; }
+        public string Rua { get; set; }
+        public string Cidade { get; set; }
+        public string Uf { get; set; }
+        public string NumResidencia { get; set; }
+
+        public Endereco()
+        {
+
+        }
+
+        public Endereco(int id, string rua, string cidade, string uf, string numResidencia)
+        {
+            Id = id;
+            Rua = rua;
+            Cidade = cidade;
+            Uf = uf;
+            NumResidencia = numResidencia;
+        }
+    }
+}
